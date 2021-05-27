66ded02c46506c6983468b07c5211a6f856f3bad
Tom Coleman <tom@thesnail.org>
2021-05-27 20:22:05 +1000

    Update for GitHub

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ function getStatus(build) {
   switch (build.status) {
     case 'FAILED':
       return {
-        state: 'failed',
+        state: 'error',
         description: 'Build ${build.number} has suffered a system error. Please try again.',
       };
 
     case 'BROKEN':
       return {
-        state: 'failed',
+        state: 'failure',
         description: `Build ${build.number} failed to render.`,
       };
     case 'DENIED':
       return {
-        state: 'failed',
+        state: 'failure',
         description: `Build ${build.number} denied.`,
       };
     case 'PENDING':
@@ -48,26 +48,19 @@ function getStatus(build) {
 async function setCommitStatus(build, repoId) {
   const status = getStatus(build);
 
-  console.log(build);
-  console.log(status);
-
-  const queryString = stringify({
+  const body = JSON.stringify({
     context: 'UI Tests',
     target_url: build.webUrl,
     ...status,
   });
 
-  console.log(`POSTING to ${REST_API}projects/${repoId}/statuses/${build.commit}?${queryString}`);
+  console.log(`POSTING to ${REST_API}repositories/${repoId}/statuses/${build.commit}`);
 
-  const result = await fetch(
-    `${REST_API}projects/${repoId}/statuses/${build.commit}?${queryString}`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${TOKEN}`,
-      },
-    }
-  );
+  const result = await fetch(`${REST_API}repositories/${repoId}/statuses/${build.commit}`, {
+    method: 'POST',
+    body,
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  });
 
   console.log(result);
   console.log(await result.text());
